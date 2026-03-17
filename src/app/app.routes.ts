7b5946d417627894364634f409c6317db1665f73
Amilkar Guarachi Nina <guamilkar@gmail.com>
2026-03-17 16:45:58 -0400

    primer commit
    commit 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent }, // Ruta principal (Front)
+  { path: 'login', component: LoginComponent }, // Ruta del Login
+  { path: '**', redirectTo: '' } // Si escriben una URL que no existe, los devuelve al Front
+];
