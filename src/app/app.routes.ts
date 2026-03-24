0f95d56cb1332b57c41a3dcdbd6773425ffb2c9e
Amilkar Guarachi Nina <guamilkar@gmail.com>
2026-03-24 03:20:14 -0400

    dashboard
    se creo el dashboard principal y el mapa principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
 import { Login } from './auth/login/login';
-import { Register } from './auth/register/register'; // Verifica que esta ruta sea exacta
+import { Register } from './auth/register/register';
+import { Dashboard } from './pages/dashboard/dashboard'; // Importamos el dashboard
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'login', component: Login },
-  { path: 'register', component: Register }, // Esta es la ruta para el botón
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard }, // Nueva ruta del panel
   { path: '**', redirectTo: '' }
 ];
--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,29 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router'; // Importar Router
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  // Es muy importante importar RouterLink y ReactiveFormsModule aquí
-  imports: [RouterLink, ReactiveFormsModule], 
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
 })
 export class Login {
-  // Definimos la estructura y validaciones del formulario
+  private router = inject(Router); // Inyectar el servicio de rutas
+
   loginForm = new FormGroup({
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required)
   });
 
-  // Esta función se ejecutará al hacer clic en "Ingresar"
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Listo para enviar al Backend:', this.loginForm.value);
-      // En el siguiente paso aquí conectaremos con Django
-    } else {
-      console.log('El formulario no es válido');
+      console.log('Login exitoso (Simulado)');
+      this.router.navigate(['/dashboard']); // Redirección con animación
     }
   }
 }
